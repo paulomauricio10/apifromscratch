d3a5d0488d4198ca7439fce6e84f5f5c6bcecf3a
paulomauricio10 <paulomauricio10@gmail.com>
2026-09-21 15:50:53 -0300

    Add comment to ApiApplicationTests
    Added a new comment in the ApiApplicationTests class.

--- src/test/java/com/example/api/ApiApplicationTests.java
+++ src/test/java/com/example/api/ApiApplicationTests.java
@@ -3,6 +3,7 @@ package com.example.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//new comment
 @SpringBootTest
 class ApiApplicationTests {
 
